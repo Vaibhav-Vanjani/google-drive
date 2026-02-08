0776914d6958d9c30d1d7cc11cd17f7c8c7b2f5b
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2026-02-08 18:32:03 +0530

    v1 changes updated

--- server.js
+++ server.js
@@ -3,29 +3,81 @@ import http from 'node:http';
 import fs from 'node:fs/promises';
 import { createReadStream } from 'node:fs';
 
+async function serveDir({path,req,res}){
+    try {
+         const files = await readdir(path);
+
+        let fileHtmlList = "";
+        for(const file of files){
+            console.log("path->",path);
+            console.log("file->",file);
+            
+            const stats = await fs.stat(`${path}/${file}`);
+           let folderpath = path.replace('.','');
+            if(stats.isFile()){
+                fileHtmlList+=`<li><a href="${folderpath}/${file}">${file}</a> <a href="${folderpath}/${file}_download">Download</a>`
+            }
+            else if(stats.isDirectory()){
+                fileHtmlList+=`<li><a href="${folderpath}/${file}">${file}</a>`
+            }
+        }
+
+        let content = await fs.readFile("./index.html", "utf8");
+        content = content.replace('${dynamicContent}',fileHtmlList);
+
+        res.end(content);
+    } catch (error) {
+        console.log(error);
+        res.end('NOT FOUND 404');
+    }
+   
+}
+
+function serveFile({path,req,res}){
+     try {
+         let requestUrl = req.url;
+         if(requestUrl.includes('_download')){
+             requestUrl = requestUrl.replace('_download','');
+             const fileName = requestUrl.split('/').at(-1);
+             res.setHeader("Content-Disposition", `attachment; filename="${fileName}"`);                
+         }
+         else if(requestUrl.includes('.mp4')){
+            res.setHeader('content-type',"video/mp4");
+         }
+         console.log(requestUrl);
+         const readstream = createReadStream(`.${requestUrl}`);
+         readstream.pipe(res);
+     } catch (error) {
+        console.log(error);
+         res.end("Not Found");
+     }
+}
+
+
 const server = http.createServer(async(req,res)=>{
     if(req.url === '/favicon.ico')return;
 
     if(req.url !== '/'){
-        const readstream = createReadStream(`./storage${req.url}`);
-        readstream.pipe(res);
+        try {
+            const fileTypePath = req.url.replace('_download','');
+            const stats = await fs.stat(`.${fileTypePath}`);
+            if(stats.isFile()){
+                serveFile({path:`.${req.url}`,req,res});
+            }
+            else{
+                serveDir({path:`.${req.url}`,req,res});
+            }
+        } catch (error) {
+            console.log(error);
+            res.end('404 NOT FOUND');
+        }
+        
         return;
     }
 
-    const files = await readdir('./storage');
-
-    let fileHtmlList = "";
-    for(const file of files){
-        fileHtmlList+=`<li><a href="${file}">${file}</a> <button>Download</button></li>`
-    }
-
-    // res.setHeader('Content-Type','text/html');
-    
-    let content = await fs.readFile("./index.html", "utf8");
-    content = content.replace('${dynamicContent}',fileHtmlList);
-
-    res.end(content);
+    serveDir({path:'./storage',req,res});
 })
+
 
 server.listen(4000,'0.0.0.0',()=>{
     console.log("Server started at 4000");
